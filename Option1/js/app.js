922fbfb25193879a0ccea96fe627fe920cf0ae5a
ludjoh <ludjoh734@ihmeducation.se>
2024-09-30 11:26:54 +0200

    Cookies

--- Option1/js/app.js
+++ Option1/js/app.js
@@ -1,4 +1,3 @@
-
 let button0Reference = document.getElementById("button0");
 
 let playerRoll = 0;
@@ -15,45 +14,30 @@ let scoreboardPlayer = document.getElementById("scoreboardPlayer");
 let scoreboardTies = document.getElementById("scoreboardTies");
 let scoreboarda1 = document.getElementById("scoreboarda1");
 
-button0Reference.addEventListener("click", function() {
-  getRandomNumberForPlayer();
-  showPlayerRollText();
-  getRandomNumberFora1();
-  showa1RollText();
-  showResultRoll();
-  updateScoreboard();
-  showUpdatedScoreboard();
-});
-
-
 function getRandomNumberForPlayer() {
   playerRoll = Math.floor(Math.random() * 6) + 1;
 }
 
 function showPlayerRollText() {
-  playerRollText.innerText = "Spelarens kast blev: "+playerRoll+"";
+  playerRollText.innerText = "Spelarens kast blev: " + playerRoll;
 }
+
 function getRandomNumberFora1() {
   a1Roll = Math.floor(Math.random() * 6) + 1;
 }
 
 function showa1RollText() {
-  a1RollText.innerText = "Datorns kast blev: "+a1Roll+"";
+  a1RollText.innerText = "Datorns kast blev: " + a1Roll;
 }
 
 function updateScoreboard() {
-if (playerRoll > a1Roll) {
-  playerWins++;
-  return scoreboardPlayer;
-}
-if (playerRoll < a1Roll) {
-  a1Wins++;
- return scoreboarda1;
-}
-if (playerRoll === a1Roll) {
-  ties++;
-  return scoreboardTies;
-}
+  if (playerRoll > a1Roll) {
+    playerWins++;
+  } else if (playerRoll < a1Roll) {
+    a1Wins++;
+  } else {
+    ties++;
+  }
 }
 
 function showUpdatedScoreboard() {
@@ -68,9 +52,59 @@ function showResultRoll() {
     resultRoll.style.color = "red";
   } else if (playerRoll > a1Roll) {
     resultRoll.innerText = "Spelaren vinner!";
-    resultRoll.style.color = "Green";
+    resultRoll.style.color = "green";
   } else {
     resultRoll.innerText = "Oavgjort!";
-    resultRoll.style.color = "Yellow";
+    resultRoll.style.color = "yellow";
   }
 }
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+function loadCookies() {
+  playerWins = parseInt(getCookie("playerWins")) || 0;
+  a1Wins = parseInt(getCookie("a1Wins")) || 0;
+  ties = parseInt(getCookie("ties")) || 0;
+
+  showUpdatedScoreboard();
+}
+
+loadCookies();
+getCookie();
+
+button0Reference.addEventListener("click", function() {
+  getRandomNumberForPlayer();
+  showPlayerRollText();
+  getRandomNumberFora1();
+  showa1RollText();
+  showResultRoll();
+  updateScoreboard();
+  showUpdatedScoreboard();
+
+  saveCookie();
+  saveCookie();
+  saveCookie();
+});
+
+function saveCookie() {
+  document.cookie = "playerWins=" + playerWins + ";expires=Thu, 4 nov 2025 12:00:00 UTC";
+  document.cookie = "a1Wins=" + a1Wins + ";expires=Thu, 4 nov 2025 12:00:00 UTC";
+  document.cookie = "ties=" + ties + ";expires=Thu, 4 nov 2025 12:00:00 UTC";
+}
+
+
